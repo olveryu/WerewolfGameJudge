be00213013f6efc0deb3e8ed0e4ea53a0d62decb
Yan, Edwin <olveryu@gmail.com>
2026-04-07 17:44:13 -0400

    fix(config): increase testTimeout to 10s for parallel board UI tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'jest-expo',
+  testTimeout: 10_000,
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|react-native-reanimated|react-native-gesture-handler|@shopify/react-native-skia)',
   ],
